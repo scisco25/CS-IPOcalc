b9a5161dc56e9052c4afe612e750aafdc2e5308b
scisco25 <92340877+scisco25@users.noreply.github.com>
2023-10-19 08:52:28 -0600

    add comments

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ document.getElementById('calc').addEventListener('click',calc);
 
 
 function calc(){
+    //Get Values from inputs
     let a = document.getElementById('a').value;
     let b = document.getElementById('b').value;
+    //calculate the result and set the output to the result
     document.getElementById('out').innerHTML = `${Math.sqrt((a^2 + b^2))}`
 }
