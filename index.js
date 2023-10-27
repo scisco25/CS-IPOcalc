a9d6d2734ebb20900dea66f648bd0ae760c80d02
scisco25 <92340877+scisco25@users.noreply.github.com>
2023-10-27 09:04:01 -0600

    Fix Operators, Add MORE Comments

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+//register the button listener for the calculate button to run the calc function
 document.getElementById('calc').addEventListener('click',calc);
 
-
+//calculate the pythagorean theorem using inputs
 function calc(){
-    //Get Values from inputs
+    //Get Values from inputs for A and B
     let a = document.getElementById('a').value;
     let b = document.getElementById('b').value;
-    //calculate the result and set the output to the result
-    document.getElementById('out').innerHTML = `${Math.sqrt((a^2 + b^2))}`
+    //calculate the result using the pythagorean theorem (A^2 + B^2 = C^2) and set the output to the result
+    //square A and B then square root to get C
+    document.getElementById('out').innerHTML = `${Math.sqrt((a**2 + b**2))}`
 }
